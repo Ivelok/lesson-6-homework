269f83b196331ec16ce61ba78109bd632275333e
Anton Peretrukhin <aperetrukhin@naumen.ru>
2018-07-25 19:00:05 +0500

    HomeWork 6: ivelok

--- market/settings.py
+++ market/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_)5rt=c6f@#y7sr@=1fbslz^ev^xmw3*m6(y7lgw!c^^h=(5gy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'market.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
